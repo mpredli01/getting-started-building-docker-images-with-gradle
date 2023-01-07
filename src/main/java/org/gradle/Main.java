a740bad43bd71bc78854a95722a3646e2e8ab224
Michael P. Redlich <mike@redlich.net>
2023-01-06 20:03:23 -0500

    Updates 1/6/2023

--- src/main/java/org/gradle/Main.java
+++ src/main/java/org/gradle/Main.java
@@ -1,4 +1,17 @@
 package org.gradle;
 
+import com.bmuschko.gradle.docker.DockerJavaApplication;
+import com.bmuschko.gradle.docker.DockerJavaApplicationPlugin;
+import com.bmuschko.gradle.docker.DockerRemoteApiPlugin;
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage;
+// import org.gradle.api.model.ObjectFactory;
+
 public class Main {
-}
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle Docker Plugin");
+        DockerRemoteApiPlugin dockerRemoteApiPlugin = new DockerRemoteApiPlugin();
+        DockerJavaApplicationPlugin dockerJavaApplicationPlugin = new DockerJavaApplicationPlugin();
+        DockerBuildImage dockerBuildImage = new DockerBuildImage();
+        }
+    }
--- src/test/java/org/gradle/MainTest.java
+++ src/test/java/org/gradle/MainTest.java
@@ -1,4 +1,4 @@
 package org.gradle;
 
 public class MainTest {
-}
+    }
